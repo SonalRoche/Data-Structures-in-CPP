41b53d121e14e74659244ac074daa761a9fa8abf
Sonal Roche <sonal98roche@gmail.com>
2025-02-21 20:17:25 -0500

    Added binarySearchTree.cpp

---
+++ binarySearchTree.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data) {
+        this->data = data;
+        left = nullptr;
+        right = nullptr;
+    }
+};
